edfd3793c03a41c914c8974ef2fff92b0c96fde8
lapivoinerouge <wioladys@gmail.com>
2020-02-23 03:40:40 +0100

    first commit

---
+++ src/main/java/com/parser/JsonParserModuleApplication.java
@@ -0,0 +1,13 @@
+package com.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonParserModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonParserModuleApplication.class, args);
+    }
+
+}
